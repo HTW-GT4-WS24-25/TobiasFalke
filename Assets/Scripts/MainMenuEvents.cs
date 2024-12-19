5ab55a442d1da3229caf63790442aaac34328c45
mcanti1999 <schoeps.anton@gmail.com>
2024-12-19 16:29:01 +0100

    fixed nullrefrence Error

--- Assets/Scripts/MainMenuEvents.cs
+++ Assets/Scripts/MainMenuEvents.cs
@@ -7,18 +7,18 @@ public class MainMenuEvents : MonoBehaviour
     private UIDocument _uiDocument;
     private Button _playButton;
     private Button _settingsButton;
-    private Button _exitButton;
+    private Button _quitButton;
 
     private void Awake()
     {
         _uiDocument = GetComponent<UIDocument>();
         _playButton = _uiDocument.rootVisualElement.Q("PlayButton") as Button;
         _settingsButton = _uiDocument.rootVisualElement.Q("SettingsButton") as Button;
-        _exitButton = _uiDocument.rootVisualElement.Q("ExitButton") as Button;
+        _quitButton = _uiDocument.rootVisualElement.Q("QuitButton") as Button;
         
         _playButton.RegisterCallback<ClickEvent>(OnClickPlayButton);
         _settingsButton.RegisterCallback<ClickEvent>(OnClickSettingsButton);
-        _exitButton.RegisterCallback<ClickEvent>(OnClickExitButton);
+        _quitButton.RegisterCallback<ClickEvent>(OnClickExitButton);
         
     }
 
@@ -40,6 +40,6 @@ public class MainMenuEvents : MonoBehaviour
     {
         _playButton.UnregisterCallback<ClickEvent>(OnClickPlayButton);
         _settingsButton.UnregisterCallback<ClickEvent>(OnClickSettingsButton);
-        _exitButton.UnregisterCallback<ClickEvent>(OnClickExitButton);
+        _quitButton.UnregisterCallback<ClickEvent>(OnClickExitButton);
     }
 }
