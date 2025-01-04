8a1424e94220c4e2ab59e745ff33d9a571587230
FlyingBaum <kristgsus@gmail.com>
2025-01-04 19:21:34 +0100

    feat(Scripts/Obstacle): add obstacle type + score / health logic respecting type + better naming

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,19 +1,33 @@
+using Mono.Cecil;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour, IObject
 {
-    public bool canJumpOver;
-    public bool canGrindOver;
+    [SerializeField] private ObstacleType obstacleType;
+    
+    private enum ObstacleType
+    {
+        Wall,
+        BigObstacle,
+        SmallObstacle,
+        Rail,
+        Hole
+    }
+
+    private bool IsJumpable => obstacleType != ObstacleType.Wall;
 
     public void Collide(GameObject obstacle, PlayerStats playerStats, PlayerMovement playerMovement)
     {
-        // start grinding over obstacle if possible
-        if (canGrindOver)
+        if (playerMovement.IsJumping && IsJumpable)
+        {
+            IncreaseScore(playerStats);
+            return;
+        }
+        if (obstacleType == ObstacleType.Rail)
         {
             HandleGrindCollision(playerStats);
         }
-        // trigger collision if not grinding or jumping against non-jump-able object
-        else if (!canJumpOver || !playerMovement.IsJumping)
+        else if (!IsJumpable || !playerMovement.IsJumping)
         {
             TriggerCollisionEffect(playerStats);
         }
@@ -30,9 +44,49 @@ public class Obstacle : MonoBehaviour, IObject
     private void TriggerCollisionEffect(PlayerStats playerStats)
     {
         Debug.Log("Collision!");
-        playerStats.UpdateHealth(-20); // TODO: make damage depend on type of obstacle?
         AudioManager.Instance.PlaySound("crash");
+        UpdatePlayerHealth(playerStats);
         // TODO: add animation to player when colliding
         // TODO: have player phrase through obstacle or be pushed below the screen (game over)
+    }
+    
+    private void IncreaseScore(PlayerStats playerStats)
+    {
+        int amount = 10;
+        switch (obstacleType)
+        {
+            case ObstacleType.BigObstacle:
+                amount = 50;
+                break;
+            case ObstacleType.SmallObstacle:
+                amount = 10;
+                break;
+            case ObstacleType.Hole:
+                amount = 60;
+                break;
+        }
+        playerStats.UpdateScore(amount);
+    }
+    
+    private void UpdatePlayerHealth(PlayerStats playerStats)
+    {
+        int amount = -20;
+        switch (obstacleType)
+        {
+            case ObstacleType.Wall:
+                amount = -50;
+                break;
+            case ObstacleType.BigObstacle:
+                amount = -20;
+                break;
+            case ObstacleType.SmallObstacle:
+                amount = -10;
+                break;
+            case ObstacleType.Hole:
+                amount = -100;
+                break;
+        }
+        
+        playerStats.UpdateHealth(amount);
     }
 }
