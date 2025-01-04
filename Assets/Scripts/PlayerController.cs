7f00c89b1f62e16649174755534db2de7bad5f26
FlyingBaum <kristgsus@gmail.com>
2025-01-04 20:50:43 +0100

    style(Scripts/PlayerController): capitalize comments and add period in the end

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,14 +2,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 { 
-    private PlayerStats stats; // handles player's current health, special & score
-    private PlayerMovement movement; // handles player's movement input & animation
-
+    private PlayerStats stats; // Handles player's current health, special & score.
+    private PlayerMovement movement; // Handles player's movement input & animation.
+    
     private void Start()
     {
-        stats = new PlayerStats(); // is created with default values for each stat
-        movement = GetComponent<PlayerMovement>(); // values specified in component attached within the player prefab
-        // initialize player's max health & special bars on UI
+        stats = new PlayerStats(); // Is created with default values for each stat.
+        movement = GetComponent<PlayerMovement>(); // Values specified in component attached within the player prefab.
+        
+        // Initialize player's max health & special bars on UI.
         UIManager.Instance.SetMaxHealth(stats._maxHealth);
         UIManager.Instance.SetMaxSpecial(stats._maxSpecial);
         UIManager.Instance.UpdateHealthBar(stats._health);
@@ -18,35 +19,38 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        stats._score += Time.deltaTime * 5 * stats._scoreMultiplier; // raise score continuously while game is active
+        // Increase score continuously while game is active.
+        stats._score += Time.deltaTime * 5 * stats._scoreMultiplier;
     }
 
     private void Update()
     {
-        // trigger special action if conditions are met
+        // Trigger special action if conditions are met.
         if (Input.GetKeyDown("x") && stats._special == 100) TriggerSpecialAction(); 
     }
 
     private void LateUpdate()
     {
-        // update currently shown score in UI
+        // Update currently shown score in UI.
         UIManager.Instance.UpdateScoreCounter(stats._score);
-        // show special action button when special bar is full
+        // Show special action button when special bar is full.
         if (stats._special >= 100) UIManager.Instance.ToggleSpecialActionButton(true);
     }
     
     private void TriggerSpecialAction()
     {
-        // trigger effect of special action
+        // Trigger effect of special action.
         stats.SetHealth(100);
         stats.UpdateScoreMultiplier(100);
         stats.UpdateSpeedMultiplier(50);
         stats.UpdateJumpDuration(50);
         AudioManager.Instance.PlaySound("specialAction"); 
-        // reset special points to 0
+        
+        // Reset special points to 0.
         stats._special = 0;
         UIManager.Instance.UpdateSpecialBar(0);
-        // hide button for triggering special action
+        
+        // Hide button for triggering special action.
         UIManager.Instance.ToggleSpecialActionButton(false); 
     }
 
